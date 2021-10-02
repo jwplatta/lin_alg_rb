28d7e47df5be9f0b6b89ce9fa0b3af47f75b47d9
Joseph Platta <jwplatta@gmail.com>
2021-10-01 21:59:14 -0700

    add focus to spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,4 +5,6 @@ require 'lin_alg_rb' # and any other gems you need
 
 RSpec.configure do |config|
   config.example_status_persistence_file_path = '.rspec_status'
+  config.filter_run_when_matching :focus
+  config.order = :random
 end
