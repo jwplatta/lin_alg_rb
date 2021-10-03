f7db21461ecfdf4d83006bb5d64037b4b88aa087
Joseph Platta <jwplatta@gmail.com>
2021-10-02 22:56:57 -0700

    Implement Line class

--- lib/lin_alg_rb/line.rb
+++ lib/lin_alg_rb/line.rb
@@ -1,4 +1,131 @@
 module LinAlgRb
+  class NoNoneZeroElements < StandardError
+    def initialize
+      super("No non-zero elements.")
+    end
+  end
+
   class Line
+    # NOTE: the normal vector provides the coefficients for the
+    # standard form of the line. The normal vector is used instead
+    # of the direction vector because the normal vector is easier to
+    # generalize to higher dimensions.
+    def initialize(normal_vector:, constant_term: 0)
+      @dimension = 2
+      raise ArgumentError.new("The normal vector needs to be an instance of LinAlgRb::Vector") if normal_vector.class != LinAlgRb::Vector
+      @normal_vector = normal_vector
+      @constant_term = constant_term
+      @basepoint = find_basepoint
+    end
+
+    attr_reader :dimension, :normal_vector, :constant_term, :basepoint
+
+    def parallel?(other)
+      raise ArgumentError.new("Must be instance of LinAlgRb::Line") if other.class != Line
+
+      normal_vector.parallel?(other.normal_vector)
+    end
+
+    def eql?(other)
+      self.==(other)
+    end
+
+    def ==(other)
+      raise ArgumentError.new("Must be instance of LinAlgRb::Line") if other.class != Line
+
+      if normal_vector.zero?
+        if !other.normal_vector.zero?
+          return false
+        else
+          return near_zero?(constant_term - other.constant_term)
+        end
+      elsif other.normal_vector.zero?
+        return false
+      end
+
+      return false if !parallel?(other)
+
+      (basepoint - other.basepoint).orthogonal?(normal_vector)
+    end
+
+    def intersect(other)
+      a, b = normal_vector.coordinates
+      k1 = constant_term
+
+      c, d = other.normal_vector.coordinates
+      k2 = other.constant_term
+
+      x_numerator = d*k1 - b*k2
+      y_numerator = -c*k1 + a*k2
+      one_over_denominator = 1.0 / (a*d - b*c)
+
+      (LinAlgRb::Vector.new([x_numerator, y_numerator]) * one_over_denominator)
+    rescue ZeroDivisionError => e
+      if eql?(other)
+        self
+      else
+        e
+      end
+    end
+
+    def to_s
+      decimal_places = 3
+      vars = ('a'..'z').to_a
+      init_index = index_first_nonzero_element(normal_vector.coordinates)
+
+      terms = (0...dimension).map do |index|
+        coef = normal_vector[index].round(decimal_places)
+        if coef != 0
+          first_term = (index==init_index)
+          if coef % 1 == 0.0
+            coef = coef.to_i
+          end
+
+          output = ''
+
+          if coef < 0
+            output += '-'
+          elsif coef > 0 and !first_term
+            output += '+'
+          end
+
+          if !first_term
+            output += ' '
+          end
+
+          if coef.abs != 1
+            output += "#{coef.abs}"
+          end
+
+          "#{output}#{vars.shift}"
+        end
+      end.join(" ")
+
+      "#{terms} = #{constant_term.round(decimal_places)}"
+    end
+
+    def find_basepoint
+      elm_idx = index_first_nonzero_element(normal_vector.coordinates)
+      init_coef = normal_vector[elm_idx]
+
+      ([0] * dimension).then do |basepoint_coord|
+        basepoint_coord[elm_idx] = (constant_term / init_coef.to_f)
+        LinAlgRb::Vector.new(basepoint_coord)
+      end
+    rescue NoNoneZeroElements
+      @basepoint = nil
+    end
+
+    private
+
+    def index_first_nonzero_element(elements)
+      elm = elements.find_index { |e| !near_zero?(e) }
+      raise NoNoneZeroElements.new unless elm
+      elm
+    end
+
+    def near_zero?(numeric, tolerance: 1e-10)
+      numeric.abs < tolerance
+    end
   end
 end
--- spec/line_spec.rb
+++ spec/line_spec.rb
@@ -1,6 +1,84 @@
 require 'spec_helper'
 
 describe LinAlgRb::Line do
-  xit '' do
+  describe '#find_basepoint' do
+    it do
+      vect = LinAlgRb::Vector.new([3,2])
+      line = described_class.new(normal_vector: vect, constant_term: 5)
+      expected = LinAlgRb::Vector.new([(5 / 3.0), 0])
+      expect(line.basepoint).to eq expected
+    end
+  end
+
+  describe '#to_s' do
+    it do
+      vect = LinAlgRb::Vector.new([-3,-2])
+      line = described_class.new(normal_vector: vect, constant_term: 1)
+      expect(line.to_s).to eq  "-3a - 2b = 1"
+    end
+  end
+
+  describe '#parallel?' do
+    it 'returns false' do
+      line1 = described_class.new(
+        normal_vector:  LinAlgRb::Vector.new([7.204, 3.182]),
+        constant_term: 8.68
+      )
+      line2 = described_class.new(
+        normal_vector:  LinAlgRb::Vector.new([8.172, 4.114]),
+        constant_term: 9.883
+      )
+      expect(line1.parallel?(line2)).to eq false
+    end
+    it 'returns true' do
+      line1 = described_class.new(
+        normal_vector:  LinAlgRb::Vector.new([1.182, 5.562]),
+        constant_term: 6.744
+      )
+      line2 = described_class.new(
+        normal_vector:  LinAlgRb::Vector.new([1.773, 8.343]),
+        constant_term: 9.525
+      )
+      expect(line1.parallel?(line2)).to eq true
+    end
+  end
+  describe '#eql?' do
+    it 'returns true' do
+      line1 = described_class.new(
+        normal_vector:  LinAlgRb::Vector.new([4.046, 2.836]),
+        constant_term: 1.21
+      )
+      line2 = described_class.new(
+        normal_vector:  LinAlgRb::Vector.new([10.115, 7.09]),
+        constant_term: 3.025
+      )
+      expect(line1.eql?(line2)).to eq true
+    end
+    it 'returns false' do
+      line1 = described_class.new(
+        normal_vector:  LinAlgRb::Vector.new([1.182, 5.562]),
+        constant_term: 6.744
+      )
+      line2 = described_class.new(
+        normal_vector:  LinAlgRb::Vector.new([1.773, 8.343]),
+        constant_term: 9.525
+      )
+      expect(line1.eql?(line2)).to eq false
+    end
+  end
+  describe '#intersect' do
+    it 'returns true' do
+      line1 = described_class.new(
+        normal_vector:  LinAlgRb::Vector.new([7.204, 3.182]),
+        constant_term: 8.68
+      )
+      line2 = described_class.new(
+        normal_vector:  LinAlgRb::Vector.new([8.172, 4.114]),
+        constant_term: 9.883
+      )
+      result = line1.intersect(line2).coordinates.map { |c| c.round(3) }
+
+      expect(result).to eq [1.173, 0.073]
+    end
   end
 end
