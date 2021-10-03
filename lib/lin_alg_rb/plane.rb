f18489a57a28ef87b894ba9614d3238c23428e11
Joseph Platta <jwplatta@gmail.com>
2021-10-02 22:57:08 -0700

    Implement Plane class

--- lib/lin_alg_rb/plane.rb
+++ lib/lin_alg_rb/plane.rb
@@ -1,4 +1,91 @@
 module LinAlgRb
+  class NoNonZeroElements < StandardError
+    def initialize
+      super("No non-zero elements.")
+    end
+  end
+
   class Plane
+    def initialize(normal_vector:, constant_term: 0)
+      raise ArgumentError.new("The normal vector needs to be an instance of LinAlgRb::Vector") if normal_vector.class != LinAlgRb::Vector
+      @normal_vector = normal_vector
+      @dimension = normal_vector.dimension
+      @constant_term = constant_term
+      @basepoint = find_basepoint
+    end
+
+    attr_reader :dimension, :normal_vector, :constant_term, :basepoint
+
+    def parallel?(other)
+      normal_vector.parallel?(other.normal_vector, to_decimal: 7)
+    end
+
+    def eql?(other)
+      self.==(other)
+    end
+
+    def ==(other)
+      (basepoint - other.basepoint).orthogonal?(normal_vector)
+    end
+
+    def to_s
+      decimal_places = 3
+      vars = ('a'..'z').to_a
+      init_index = index_first_nonzero_element(normal_vector.coordinates)
+
+      terms = (0...dimension).map do |index|
+        coef = normal_vector[index].round(decimal_places)
+        if coef != 0
+          first_term = (index==init_index)
+          if coef % 1 == 0.0
+            coef = coef.to_i
+          end
+
+          output = ''
+
+          if coef < 0
+            output += '-'
+          elsif coef > 0 and !first_term
+            output += '+'
+          end
+
+          if !first_term
+            output += ' '
+          end
+
+          if coef.abs != 1
+            output += "#{coef.abs}"
+          end
+
+          "#{output}#{vars.shift}"
+        end
+      end.join(" ")
+
+      "#{terms} = #{constant_term.round(decimal_places)}"
+    end
+
+    def find_basepoint
+      elm_idx = index_first_nonzero_element(normal_vector.coordinates)
+      init_coef = normal_vector[elm_idx]
+
+      ([0] * dimension).then do |basepoint_coord|
+        basepoint_coord[elm_idx] = (constant_term / init_coef.to_f)
+        LinAlgRb::Vector.new(basepoint_coord)
+      end
+    rescue NoNonZeroElements
+      @basepoint = nil
+    end
+
+    private
+
+    def index_first_nonzero_element(elements)
+      elm = elements.find_index { |e| !near_zero?(e) }
+      raise NoNoneZeroElements.new unless elm
+      elm
+    end
+
+    def near_zero?(numeric, tolerance: 1e-10)
+      numeric.abs < tolerance
+    end
   end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,6 +1,66 @@
 require 'spec_helper'
 
 describe LinAlgRb::Plane do
-  xit '' do
+  describe '#find_basepoint' do
+    it do
+      vect = LinAlgRb::Vector.new([3,2,1])
+      line = described_class.new(normal_vector: vect, constant_term: 5)
+      expected = LinAlgRb::Vector.new([(5 / 3.0), 0, 0])
+      expect(line.basepoint).to eq expected
+    end
+  end
+
+  describe '#to_s' do
+    it do
+      vect = LinAlgRb::Vector.new([-3,-2, 4])
+      line = described_class.new(normal_vector: vect, constant_term: 1)
+      expect(line.to_s).to eq  "-3a - 2b + 4c = 1"
+    end
+  end
+  describe 'parallel and equal planes' do
+    let(:plane3) do
+      described_class.new(
+        normal_vector: LinAlgRb::Vector.new([2.611, 5.528, 0.283]),
+        constant_term: 4.6
+      )
+    end
+    let(:plane4) do
+      described_class.new(
+        normal_vector: LinAlgRb::Vector.new([7.715, 8.306, 5.342]),
+        constant_term: 3.76
+      )
+    end
+    describe '#parallel?' do
+      it 'returns true' do
+        plane1 = described_class.new(
+          normal_vector: LinAlgRb::Vector.new([-7.926, 8.625, -7.212]),
+          constant_term: -7.952
+        )
+        plane2 = described_class.new(
+          normal_vector: LinAlgRb::Vector.new([-2.642, 2.875, -2.404]),
+          constant_term: -2.443
+        )
+        expect(plane1.parallel?(plane2)).to be true
+      end
+      it 'returns false' do
+        expect(plane3.parallel?(plane4)).to be false
+      end
+    end
+    describe '#eql?' do
+      it 'returns true' do
+        plane1 = described_class.new(
+          normal_vector: LinAlgRb::Vector.new([-0.412, 3.806, 0.728]),
+          constant_term: -3.46
+        )
+        plane2 = described_class.new(
+          normal_vector: LinAlgRb::Vector.new([1.03, -9.515, -1.82]),
+          constant_term: 8.65
+        )
+        expect(plane1.eql?(plane2)).to be true
+      end
+      it 'returns false' do
+        expect(plane3.eql?(plane4)).to be false
+      end
+    end
   end
 end
