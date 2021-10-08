3d8d785869b1b09a286ac9ccfbdeb476efbede35
Joseph Platta <jwplatta@gmail.com>
2021-10-07 18:34:37 -0700

    Implement Matrix class

--- lib/lin_alg_rb/matrix.rb
+++ lib/lin_alg_rb/matrix.rb
@@ -1,29 +1,248 @@
 module LinAlgRb
+  class MatricesDifferentShapes < StandardError
+    def initialize(arg=nil)
+      if arg
+        super("Matrices are not the same shape: #{arg}")
+      else
+        super("Matrices are not the same shape.")
+      end
+    end
+  end
+
+  class MatrixNotSquare < StandardError
+    def initialize
+      super("Matrix is not a square.")
+    end
+  end
+
+  class MatrixHasNoInverse < StandardError
+    def initialize
+      super("Cannot invert matrix.")
+    end
+  end
+
   class Matrix
     class << self
-      def vector_to_matrix(vector)
-        raise ArgumentError.new("#{vector} is not a LingAlgRb::Vector") unless vector.is_a? LinAlgRb::Vector
-        new(vector.coordinates)
+      def identity(n)
+        coordinates = []
+
+        (0...n).each do |row_idx|
+          row = []
+          (0...n).each do |col_idx|
+            if row_idx == col_idx
+              row << 1
+            else
+              row << 0
+            end
+          end
+          coordinates << row
+        end
+
+        new(coordinates)
       end
     end
 
-    def initialize(coordinates)
-      @coordinates = [coordinates]
+    def initialize(args=nil)
+      @coordinates = if args&.first.is_a? LinAlgRb::Vector
+        unless args.find { |c| args[0].coordinates.count != c.coordinates.count }.nil?
+          raise ArgumentError.new("Vectors must be the same size.")
+        end
+
+        args.map(&:coordinates)
+      elsif args&.first.is_a? Array
+        unless args.find { |c| c.count != args[0].count }.nil?
+          raise ArgumentError.new("Elements must be the same size.")
+        end
+
+        args
+      else
+        [[]]
+      end
     end
 
     attr_reader :coordinates
 
-    def [](*index)
-      if index.size == 1
-        index[0]
-        coordinates[index[0]]
+    def shape
+      @shape ||= [coordinates.size, coordinates[0].size]
+    end
+
+    def multiply(other)
+      self.*(other)
+    end
+
+    def *(other)
+      if other.is_a? Numeric
+        coordinates.map do |row|
+          row.map { |col| col * other }
+        end.then do |mult_coordinates|
+          Matrix.new(mult_coordinates)
+        end
+      elsif other.is_a? Matrix
+        raise ArgumentError.new("element-wise matrix multiplication not implemented")
+      end
+    end
+
+    def matmul(other)
+      if shape[1] != other.shape[0]
+        raise MatricesDifferentShapes.new("Row size in first matrix not equal to column size in second matrix.")
+      end
+      col_size = other.shape[0]
+      new_coordinates = []
+      other_coordinates = other.coordinates
+
+      new_coordinates = coordinates.map do |row|
+        (0...other.shape[1]).to_a.map do |j_idx|
+          (0...col_size).inject(0) do |sum, k_idx|
+            sum + (row[k_idx] * other_coordinates[k_idx][j_idx])
+          end
+        end
+      end
+
+      Matrix.new(new_coordinates)
+    end
+
+    def add(other)
+      self.+(other)
+    end
+
+    def +(other)
+      raise MatricesDifferentShapes.new if shape != other.shape
+
+      new_coordinates = []
+      coordinates.each_with_index do |row, row_idx|
+        column = []
+        row.each_with_index do |col, col_idx|
+          column << (col + other[row_idx][col_idx])
+        end
+        new_coordinates << column
+      end
+
+      Matrix.new(new_coordinates)
+    end
+
+    def subtract(other)
+      self.-(other)
+    end
+
+    def -(other)
+      raise MatricesDifferentShapes.new if shape != other.shape
+
+      new_coordinates = []
+      coordinates.each_with_index do |row, row_idx|
+        column = []
+        row.each_with_index do |col, col_idx|
+          column << (col - other[row_idx][col_idx])
+        end
+        new_coordinates << column
+      end
+
+      Matrix.new(new_coordinates)
+    end
+
+    def transpose
+      (0...shape[1]).to_a.map do |col_idx|
+        # NOTE: #get_cols essentially turns the column into a row vector
+        get_cols(col_idx)
+      end.then do |rows|
+        Matrix.new(rows)
+      end
+    end
+
+    def diagonal
+      cnt = (shape[0] < shape[1] ? shape[0] : shape[1])
+
+      (0...cnt).map { |idx| coordinates[idx][idx] }
+    end
+
+    def trace
+      diagonal.sum
+    end
+
+    def inverse
+      raise MatrixNotSquare.new unless self.square?
+
+      if shape[0] == 1
+        Matrix.new([[1 / coordinates[0][0].to_f]])
+      elsif shape[0] == 2
+        a = coordinates[0][0]
+        b = coordinates[0][1]
+        c = coordinates[1][0]
+        d = coordinates[1][1]
+
+        raise MatrixHasNoInverse.new if (a * d) == (b * c)
+
+        Matrix.new([[d, -b],[-c, a]]) * (1 / determinant.to_f)
       else
-        puts index.class
-        nil
+        raise StandardError.new("Cannot find inverse of matrices with dimension greater than 2.")
+      end
+    end
+
+    def determinant
+      raise MatrixNotSquare.new unless self.square?
+      # NOTE: [[a, b], [c, d]] => ad - bc
+      coordinates[0][0] * coordinates[1][1] - coordinates[0][1] * coordinates[1][0]
+    end
+
+    def square?
+      shape[0] == shape[1]
+    end
+
+    def eql?(other)
+      self.==(other)
+    end
+
+    def ==(other)
+      coordinates == other.coordinates
+    end
+
+    def [](*args, axis: 0)
+      if args.size == 1 and axis == 0
+        get_rows(args.first)
+      elsif args.size == 1 and axis == 1
+        get_cols(args.first)
+      elsif args.size == 2 and axis == 0
+        row_indices, col_indices = args
+        get_rows(row_indices).then do |subset|
+          Matrix.new(subset).get_cols(col_indices)
+        end
+      elsif args.size == 2 and axis == 1
+        col_indices, row_indices = args
+        get_cols(col_indices).then do |subset|
+          Matrix.new(subset).get_rows(row_indices)
+        end
+      else
+        raise ArgumentError.new("Too many indices or wrong axis.")
+      end
+    end
+
+    def get_cols(indices)
+      if indices.is_a? Range
+        first_col = indices.begin
+        num_cols = indices.size
+        coordinates.map { |row| row[first_col, num_cols] }
+      elsif indices.is_a? Array
+        coordinates.map do |row|
+          row.select.with_index { |col, idx| indices.include? idx }
+        end
+      else
+        coordinates.map { |row| row[indices] }
+      end
+    end
+
+    def get_rows(indices)
+      if indices.is_a? Range
+        first_row = indices.begin
+        num_rows = indices.size
+        coordinates[first_row, num_rows]
+      elsif indices.is_a? Array
+        coordinates.select.with_index { |row, idx| indices.include? idx }
+      else
+        coordinates[indices]
       end
     end
 
     def to_s
+      coordinates.map { |row| "#{row.join(' ')}" }.join("\n")
     end
   end
 end
--- spec/matrix_spec.rb
+++ spec/matrix_spec.rb
@@ -1,12 +1,201 @@
 require 'spec_helper'
 
 describe LinAlgRb::Matrix do
-  describe '#[]' do
-    it 'return value at index' do
-      coordinates = [1,2,3,4]
-      matrix = described_class.new(coordinates)
+  describe '#initialize' do
+    context 'empty coordinates' do
+      it 'it returns an empty matrix' do
+        matrix = described_class.new
+        expect(matrix.coordinates).to eq [[]]
+      end
+    end
+    context 'coordinates from vectors' do
+      it 'raises when vectors are different sizes' do
+        expect do
+          described_class.new([LinAlgRb::Vector.new([1,2]), LinAlgRb::Vector.new([3,4,5])])
+        end.to raise_error(ArgumentError)
+      end
+      it 'it returns correct coordinates' do
+        matrix = described_class.new([LinAlgRb::Vector.new([1,2]), LinAlgRb::Vector.new([3,4])])
+        expect(matrix.coordinates).to eq [[1, 2], [3, 4]]
+      end
+    end
+    context 'coordinates from arrays' do
+      it 'raises when vectors are different sizes' do
+        expect do
+          described_class.new([[1,2], [3,4,5]])
+        end.to raise_error(ArgumentError)
+      end
+      it 'it returns correct coordinates' do
+        matrix = described_class.new([[1,2], [3,4]])
+        expect(matrix.coordinates).to eq [[1, 2], [3, 4]]
+      end
+    end
+  end
+  describe 'matrix equality' do
+    it 'returns true' do
+      matrix1 = described_class.new([[1,2,5], [3,4,7],[1,9,2]])
+      matrix2 = described_class.new([[1,2,5], [3,4,7],[1,9,2]])
+      expect(matrix1 == matrix2).to eq true
+    end
+    it 'returns false' do
+      matrix1 = described_class.new([[1,2,5], [3,4,7],[1,9,2]])
+      matrix2 = described_class.new([[1,2], [3,4],[1,9]])
+      expect(matrix1 == matrix2).to eq false
+    end
+  end
+  describe 'scalar multiplication' do
+    it do
+      matrix = described_class.new([[1,2,5], [3,4,7],[1,9,2]]) * 5
+      expected_matrix = described_class.new([[5, 10, 25], [15, 20, 35], [5, 45, 10]])
+      expect(matrix).to eq expected_matrix
+    end
+  end
+  describe 'matrix addition' do
+    it 'raises error when different shapes' do
+      matrix1 = described_class.new([[1,2,5], [3,4,7],[1,9,2]])
+      matrix2 = described_class.new([[3,3], [4,4],[2,2]])
+      expect do
+        matrix1 + matrix2
+      end.to raise_error(LinAlgRb::MatricesDifferentShapes)
+    end
+    it do
+      matrix1 = described_class.new([[1,2,5], [3,4,7],[1,9,2]])
+      matrix2 = described_class.new([[3,3,3], [4,4,4],[2,2,2]])
+      expected_matrix = described_class.new([[4,5,8], [7,8,11],[3,11,4]])
+      expect(matrix1 + matrix2).to eq expected_matrix
+    end
+  end
+  describe 'matrix subtraction' do
+    it 'raises error when different shapes' do
+      matrix1 = described_class.new([[1,2,5], [3,4,7],[1,9,2]])
+      matrix2 = described_class.new([[3,3], [4,4],[2,2]])
+      expect do
+        matrix1 - matrix2
+      end.to raise_error(LinAlgRb::MatricesDifferentShapes)
+    end
+    it do
+      matrix1 = described_class.new([[1,2,5], [3,4,7],[1,9,2]])
+      matrix2 = described_class.new([[3,3,3], [4,4,4],[2,2,2]])
+      expected_matrix = described_class.new([[-2,-1,2], [-1,0,3],[-1,7,0]])
+      expect(matrix1 - matrix2).to eq expected_matrix
+    end
+  end
+  describe '#matmul' do
+    it do
+      matrix1 = described_class.new([[17,25,6,2],[6,1,97,4],[80,8,54,15]])
+      matrix2 = described_class.new([[3,14,1,7,42,5],[32,11,2,4,18,17],[19,81,4,8,5,10],[27,2,3,6,7,3]])
+      expected_matrix = described_class.new([[1019,1003,97,279,1208,576],[2001,7960,408,846,783,1029],[1927,5612,357,1114,3879,1121]])
 
-      expect(matrix[0]).to eq(coordinates)
+      expect(matrix1.matmul(matrix2)).to eq expected_matrix
+    end
+  end
+  describe '#transpose' do
+    it do
+      matrix = described_class.new([[5,4,1,7], [2,1,3,5]])
+      expected_matrix = described_class.new([[5,2],[4,1],[1,3],[7,5]])
+      expect(matrix.transpose).to eq expected_matrix
+    end
+  end
+  describe '#diagonal' do
+    it do
+      matrix = described_class.new([[5,4,1,7], [2,1,3,5]])
+      expect(matrix.diagonal).to eq [5, 1]
+    end
+  end
+  describe '#trace' do
+    it do
+      matrix = described_class.new([[5,4,1,7], [2,1,3,5]])
+      expect(matrix.trace).to eq 6
+    end
+  end
+  describe '#inverse' do
+    context 'dimension 1' do
+      it do
+        matrix = described_class.new([[100]])
+        expected_matrix = described_class.new([[1 / 100.0]])
+        expect(matrix.inverse).to eq expected_matrix
+      end
+    end
+    context 'dimension 2' do
+      it do
+        matrix = described_class.new([[4, 5], [7, 1]])
+        expected_matrix = described_class.new([[-0.03225806451612903, 0.16129032258064516], [0.22580645161290322, -0.12903225806451613]])
+        expect(matrix.inverse).to eq expected_matrix
+      end
+    end
+    context 'no inverse' do
+      it 'it raises' do
+        expect do
+          described_class.new([[4, 2], [14, 7]]).inverse
+        end.to raise_error(LinAlgRb::MatrixHasNoInverse)
+      end
+    end
+  end
+  describe '.identity' do
+    it 'creates identity matrix for n equals 1' do
+      matrix = LinAlgRb::Matrix.identity(1)
+      expected_matrix = described_class.new([[1]])
+      expect(matrix).to eq expected_matrix
+    end
+    it 'creates identity matrix for n equals 2' do
+      matrix = LinAlgRb::Matrix.identity(2)
+      expected_matrix = described_class.new([[1,0], [0,1]])
+      expect(matrix).to eq expected_matrix
+    end
+    it 'creates identity matrix for n equals 4' do
+      matrix = LinAlgRb::Matrix.identity(4)
+      expected_matrix = described_class.new([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]])
+      expect(matrix).to eq expected_matrix
+    end
+  end
+  describe '#to_s' do
+    it do
+      matrix = described_class.new([[1,2,5], [3,4,7],[1,9,2]])
+      expect(matrix.to_s).to eq "1 2 5\n3 4 7\n1 9 2"
+    end
+  end
+  describe '#[]' do
+    context 'get row' do
+      it do
+        matrix = described_class.new([[17,25,6,2],[6,1,97,4],[80,8,54,15]])
+        expect(matrix[1]).to eq [6,1,97,4]
+      end
+    end
+    context 'get range of rows' do
+      it do
+        matrix = described_class.new([[17,25,6,2],[6,1,97,4],[80,8,54,15]])
+        expect(matrix[(1..2)]).to eq [[6,1,97,4],[80,8,54,15]]
+      end
+    end
+    context 'get list of rows' do
+      it do
+        matrix = described_class.new([[17,25,6,2],[6,1,97,4],[80,8,54,15]])
+        expect(matrix[[0,2]]).to eq [[17,25,6,2],[80,8,54,15]]
+      end
+    end
+    context 'get column' do
+      it do
+        matrix = described_class.new([[17,25,6,2],[6,1,97,4],[80,8,54,15]])
+        expect(matrix[1, axis: 1]).to eq [25,1,8]
+      end
+    end
+    context 'get range of columns' do
+      it do
+        matrix = described_class.new([[17,25,6,2],[6,1,97,4],[80,8,54,15]])
+        expect(matrix[(1..2), axis: 1]).to eq [[25,6],[1,97],[8,54]]
+      end
+    end
+    context 'get list of columns' do
+      it do
+        matrix = described_class.new([[17,25,6,2],[6,1,97,4],[80,8,54,15]])
+        expect(matrix[[1,3], axis: 1]).to eq [[25,2],[1,4],[8,15]]
+      end
+    end
+    context 'get subset' do
+      it do
+        matrix = described_class.new([[17,25,6,2],[6,1,97,4],[80,8,54,15]])
+        expect(matrix[[1,2], (1..2)]).to eq [[1,97],[8,54]]
+      end
     end
   end
 end
